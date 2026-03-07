bf75446e296c33b76c2ee0154852235cf9ecfaaf
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-07 17:18:45 +0000

    Set up authProvider and hook

---
+++ src/services/auth/AuthProvider.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase-service/firebase-client";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+
+  if (!ctx) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return ctx;
+};
+
+// In React, children represents whatever components are wrapped inside a component.
+// Conceptually, the provider allows everything inside of it to access the shared state
+// AuthProvider -> React state and Firebase auth listener
+// Manages auth state
