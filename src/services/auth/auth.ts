499a1babf17c5c1e85d7a24fc6307e1e259985c6
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-07 17:18:31 +0000

    Create login/log out actions

---
+++ src/services/auth/auth.ts
@@ -0,0 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../firebase-service/firebase-client";
+
+export const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    console.log("User signed in:", user);
+  } catch (error) {
+    console.error("Error signing in:", error);
+  }
+};
+
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+    console.log("User signed out");
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
+};
+
+// Auth services uses auth
+// Performs auth actions
