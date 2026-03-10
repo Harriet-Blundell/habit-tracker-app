c2516b5853b09521a7be3fc48840f69ec40daea8
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-10 21:38:29 +0000

    Add font style and apply it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
-import "./globals.css";
 import { AuthProvider } from "@/services/auth/AuthProvider";
+import "./globals.css";
+import { Press_Start_2P } from 'next/font/google';
+
+const pressStart2P = Press_Start_2P({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-press-start",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={pressStart2P.variable}>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
