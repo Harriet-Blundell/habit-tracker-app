229ef6684e8844e8d78cf3ca573837558b5a2279
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-11 21:11:34 +0000

    Add functionality to read data from firestore; iterate through the data in the UI and display it on the screen and updates as the user presses enter

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -2,12 +2,32 @@
 
 import { signOutUser } from "@/services/auth/auth";
 import { useAuth } from "@/services/auth/AuthProvider";
-import { createHabit } from "@/services/habits/habits";
-import { useState } from "react";
+import { createHabit, getHabits } from "@/services/habits/habits";
+import { useEffect, useState } from "react";
+
+export type HabitType = {
+  id: string;
+  name: string;
+};
 
 export default function Home() {
   const { user, loading } = useAuth();
-  const [habit, setHabit] = useState("");
+  const [habitInput, setHabitInput] = useState("");
+  const [habits, setHabits] = useState<HabitType[]>([]);
+
+  useEffect(() => {
+    if (!user) return;
+    const fetchHabits = async () => {
+      try {
+        const habits = await getHabits(user.uid);
+        setHabits(habits);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchHabits();
+  }, [user]);
 
   const handleSignOut = async () => {
     try {
@@ -19,17 +39,24 @@ export default function Home() {
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setHabit(value);
+    setHabitInput(value);
   };
 
   const handleSubmit = async () => {
-    const trimmedHabit = habit.trim();
+    const trimHabitInput = habitInput.trim();
     if (!user) return;
-    if (!trimmedHabit) return;
+    if (!trimHabitInput) return;
 
     try {
-      await createHabit(user.uid, habit);
-      setHabit("");
+      const createdHabit = await createHabit(user.uid, trimHabitInput);
+      setHabitInput("");
+      setHabits((prev) => [
+        ...prev,
+        {
+          id: createdHabit.id,
+          name: trimHabitInput,
+        },
+      ]);
     } catch (error) {
       console.error(error);
     }
@@ -42,7 +69,16 @@ export default function Home() {
       {/* Top: Hello */}
       <div className="flex flex-col items-center mt-8">
         <p className="text-lg md:text-xl font-semibold">Hello,</p>
-        <p className="text-lg md:text-xl font-semibold mt-1 text-center">{user?.displayName}</p>
+        <p className="text-lg md:text-xl font-semibold mt-1 text-center">
+          {user?.displayName}
+        </p>
+      </div>
+
+      {/* Habits data */}
+      <div>
+        {habits.map((habit) => {
+          return <p key={habit.id}>{habit.name}</p>;
+        })}
       </div>
 
       {/* Middle: Add habit input and button */}
@@ -51,7 +87,7 @@ export default function Home() {
           className="my-4 w-full max-w-xs rounded border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Add habit here..."
           onChange={handleOnChange}
-          value={habit}
+          value={habitInput}
           required
         />
         <button
--- src/services/habits/habits.ts
+++ src/services/habits/habits.ts
@@ -1,13 +1,37 @@
+import { HabitType } from "@/app/(protected)/page";
 import { firestore } from "../firebase-service/firebase-client";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {
+  addDoc,
+  getDocs,
+  collection,
+  serverTimestamp,
+} from "firebase/firestore";
 
 export async function createHabit(userId: string, name: string) {
   const habitsCollectionRef = collection(firestore, "users", userId, "habits");
 
-  await addDoc(habitsCollectionRef, { name, createdAt: serverTimestamp() });
+  const habit = await addDoc(habitsCollectionRef, {
+    name,
+    createdAt: serverTimestamp(),
+  });
+
+  return habit;
 }
 
-export async function getHabit(userId: string) {}
+export async function getHabits(userId: string): Promise<HabitType[]> {
+  const habitsCollectionRef = collection(firestore, "users", userId, "habits");
+
+  const snapshots = await getDocs(habitsCollectionRef);
+
+  const habitData = snapshots.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  });
+
+  return habitData as { id: string; name: string }[];
+}
 
 // Habit service layer
 
