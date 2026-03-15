9c40d445dc868f72896952ca2f314c88cb2f9cf2
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-15 10:28:59 +0000

    Setup jest to test unit tests; write formatTodaysDate and write tests

--- src/utils/formatDate.test.ts
+++ src/utils/formatDate.test.ts
@@ -1,25 +1,33 @@
-/*
-
-TODO:
-
-- Write tests to check formatTodayDate works as expected
-
-
-- it returns a string
-- it returns the correct formatted date - YYYY-MM-DD
-
-*/
-
-import { describe, it } from "node:test";
+import { formatTodaysDate } from "./formatDate";
 
 describe("formatTodaysDate", () => {
-  it("returns a string");
+  it("returns a string", () => {
+    const date = new Date("November 14, 2026");
+    const result = formatTodaysDate(date);
+    expect(typeof result).toBe("string");
+  });
 
-  it("formats a normal date correctly");
+  it("formats a normal date correctly", () => {
+    const date = new Date("November 14, 2026");
+    const result = formatTodaysDate(date);
+    expect(result).toBe("2026-11-14");
+  });
 
-  it("pads the month when it is a single digit");
+  it("pads the month when it is a single digit", () => {
+    const date = new Date("January 11, 2026");
+    const result = formatTodaysDate(date);
+    expect(result).toBe("2026-01-11");
+  });
 
-  it("pads the day when it is a single digit");
+  it("pads the day when it is a single digit", () => {
+    const date = new Date("November 1, 2026");
+    const result = formatTodaysDate(date);
+    expect(result).toBe("2026-11-01");
+  });
 
-  it("pads both month and day when both are single digits");
+  it("pads both month and day when both are single digits", () => {
+    const date = new Date("January 1, 2026");
+    const result = formatTodaysDate(date);
+    expect(result).toBe("2026-01-01");
+  });
 });
