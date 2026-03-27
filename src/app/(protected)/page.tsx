469c4ef6090290d1065a5c927b106358a6a7c352
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-03-27 21:58:06 +0000

    Refactor code to separate out Add Habit logic into its own component to be cleaner, more readable and reusable

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -15,6 +15,7 @@ import { formatTodaysDate } from "@/utils/formatDate";
 import { useEffect, useState } from "react";
 import Habit from "../components/Habit";
 import { Header } from "../components/Header";
+import { AddHabit } from "../components/AddHabit";
 
 type HabitWithTodayStatus = {
   id: string;
@@ -158,21 +159,11 @@ export default function Home() {
       </div>
 
       {/* Middle: Add habit input and button */}
-      <div className="flex flex-col items-center">
-        <input
-          className="my-4 w-full max-w-xs rounded border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Add habit here..."
-          onChange={handleOnChange}
-          value={habitValue}
-          required
-        />
-        <button
-          className="w-full max-w-xs bg-blue-500 text-white rounded p-2 mb-2 hover:bg-blue-600 transition"
-          onClick={handleSubmit}
-        >
-          Add habit
-        </button>
-      </div>
+      <AddHabit
+        habitValue={habitValue}
+        handleOnChange={(e) => handleOnChange(e)}
+        handleSubmit={handleSubmit}
+      />
 
       {/* Bottom: Sign out */}
       <div className="flex flex-col items-center mb-8">
